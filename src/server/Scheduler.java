581e2117b2d5f972a0afe9e0e025748a0bf74196
benjamincyiza <95043117+benjamincyiza@users.noreply.github.com>
2023-02-02 05:51:16 -0500

    Update Scheduler.java
    elevator updates

--- src/server/Scheduler.java
+++ src/server/Scheduler.java
@@ -4,12 +4,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Scheduler {
-	private boolean inProcess;  			// true means elevator is in process, initially is false until it gets request from floor system
+	private boolean inProcess, isAvailable, onDestination;  			// true means elevator is in process, initially is false until it gets request from floor system
 	private Queue<Object> floorRequests; 	//requests from floor system
+	private int elevatorLocation, destination;
 	
 	public Scheduler () {
 		inProcess=false;
+		isAvailable=true;
 		floorRequests= new LinkedList<>();
+		elevatorLocation=0;
+		destination=-1;
+		onDestination=false;
 		
 	}
 	
@@ -17,8 +22,8 @@ public class Scheduler {
 	
 	public synchronized void makeFloorRequest(Object request) {
 		
-		//if inProcess is true means elevator is in process and floor has to wait
-		while (inProcess) {
+		//if is not available is true means elevator is in process and floor has to wait
+		while (!isAvailable) {
             try { 
                 wait();
             } catch (InterruptedException e) {
@@ -30,6 +35,7 @@ public class Scheduler {
 		// make inProcess true
 		// and notify all threads
 		floorRequests.add(request);
+		isAvailable=false;
 		inProcess=true;
 		notifyAll();
 		
@@ -42,9 +48,9 @@ public class Scheduler {
 	// function return destination=-1 if there is no button pressed
 	
 	public synchronized int sendlamps(boolean lamps[]) {
-		int destination=-1;
 		
-		while(!inProcess) {
+		
+		while(inProcess) {
 			try { 
                 wait();
             } catch (InterruptedException e) {
@@ -56,8 +62,36 @@ public class Scheduler {
 			if(lamps[i]) {
 				destination=i;
 			}
-		}
+		}		
 		return destination;
+	}
+	
+	public synchronized void sendElevatorUpdates(int level) {
+		while(!inProcess) {
+			try { 
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println(e);
+            }
+		}
+		elevatorLocation=level;
+		 if(elevatorLocation==destination) {
+			 onDestination=true;
+			 inProcess=false;
+			 isAvailable=true;
+			 destination=-1;		 
+		 }	
+		 
+		 notifyAll();
+		
+	}
+	
+	public int getElevatorUpdates() {
+		return elevatorLocation;
+	}
+	
+	public boolean isOnDestinatiom() {
+		return onDestination;
 	}
 	
 	
