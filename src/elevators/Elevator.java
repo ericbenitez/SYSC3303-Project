982336318601212d5bf2dcd56276308178166879
Abdul Kazal <abdul.kazal@gmail.com>
2023-02-01 19:41:22 -0500

    Skeleton of Elevator thread class

---
+++ src/elevators/Elevator.java
@@ -0,0 +1,17 @@
+package elevators;
+
+/**
+ * Elevator class which represents the
+ * cars and all the accompanying
+ * functionality.
+ *
+ * @author Abdul Kazal
+ */
+public class Elevator implements Runnable{
+
+    //Configurable attributes that can be configured during thread creation
+
+    public void run(){
+
+    }
+}
