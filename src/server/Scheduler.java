fdcebdb3dd2836208e16f84ea62e140647ae4cfc
benjamincyiza <95043117+benjamincyiza@users.noreply.github.com>
2023-02-01 21:40:39 -0500

    Update Scheduler.java

--- src/server/Scheduler.java
+++ src/server/Scheduler.java
@@ -1,7 +1,41 @@
 package server;
 
-public class Scheduler {
-	
-	
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Scheduler {
+	private boolean inProcess;  			// true means elevator is in process, initially is false until it gets request from floor system
+	private Queue<Object> floorRequests; 	//requests from floor system
+	
+	public Scheduler () {
+		inProcess=false;
+		floorRequests= new LinkedList<>();
+		
+	}
+	
+	// lamps [] booleans 
+	// iterate through array and find the destination floor
+	// function return destination=-1 if there is no button pressed
+	
+	public synchronized int sendlamps(boolean lamps[]) {
+		int destination=-1;
+		
+		while(!inProcess) {
+			try { 
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println(e);
+            }
+		}
+		
+		for(int i=0; i<lamps.length;i++) {
+			if(lamps[i]) {
+				destination=i;
+			}
+		}
+		return destination;
+	}
+	
+	
+	
 }
